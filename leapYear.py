951107cb9a7dacdbbb8003116e2c099f46ab2aa7
Jack Wright <jwrite333@gmail.com>
2021-03-03 12:55:26 -0800

    Implemented testYear method to check if a given year is a leap year

--- leapYear.py
+++ leapYear.py
@@ -1,3 +1,13 @@
+def testYear(year):
+	if year % 4 == 0:
+		if year % 100 == 0:
+			if year % 400 == 0:
+				return "Year is a leap year"
+			else:
+				return "Year is not a leap year"
+		else:
+			return "Year is a leap year"
+
 def main():
 	return
 
--- test_leapYear.py
+++ test_leapYear.py
@@ -4,6 +4,6 @@ import leapYear
 class TestCase(unittest.TestCase):
 	def testYear(self):
 		result = leapYear.testYear(2000)
-		self.assertEqual(result, "That year is a leap year")
+		self.assertEqual(result, "Year is a leap year")
 
 		return
