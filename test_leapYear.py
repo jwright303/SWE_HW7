9841e5515f05f0ad635a2db88549fafe98702d18
Jack Wright <jwrite333@gmail.com>
2021-03-03 12:56:58 -0800

    Wrote second test to check for invalid leap year

--- test_leapYear.py
+++ test_leapYear.py
@@ -6,4 +6,7 @@ class TestCase(unittest.TestCase):
 		result = leapYear.testYear(2000)
 		self.assertEqual(result, "Year is a leap year")
 
+		result = leapYear.testYear(2100)
+		self.assertEqual(result, "Year is not a leap year")
+
 		return
