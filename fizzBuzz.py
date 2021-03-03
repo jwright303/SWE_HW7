bf57c5386c9a78db494419542200d60611ff4ee8
Jack Wright <jwrite333@gmail.com>
2021-03-03 13:06:16 -0800

    Implemented checkNum method to check if a number is Fizz, Buzz, FizzBuzz, or neither

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,3 +1,15 @@
+def checkNum(num):
+	if num % 3 == 0:
+		if num % 5 == 0:
+			return "FizzBuzz"
+		else:
+			return "Fizz"
+	elif num % 5 == 0:
+		return "Buzz"
+	else:
+		return num
+	return
+
 def main():
 	return
 main()
